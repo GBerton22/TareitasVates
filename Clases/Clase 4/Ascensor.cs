42a1081b6905c4f85bb7855af196d6fa0fe10c32
Gonzalo <113288@tecnicatura.frc.utn.edu.ar>
2023-09-13 12:44:41 -0300

    s
    s

--- Clases/Clase 4/Ascensor.cs
+++ Clases/Clase 4/Ascensor.cs
@@ -1,6 +1,8 @@
 namespace Clase_4
 {
     using System;
+    using System.Runtime.InteropServices;
+
     // Un ascensor posee una capacidad máxima de n personas y 
     // está instalado en un edificio cuyos pisos se encuentran numerados.
     // Se necesita desarrollar una clase Ascensor 
@@ -28,12 +30,14 @@ namespace Clase_4
 
         public bool Desplazar(int pisoDestino)
         {
-            if (pisoDestino < -2 && pisoDestino > 10)
+            if (pisoDestino < -2 && pisoDestino > 10){
                 Console.WriteLine("El piso esta fuera de rango...");
                 return false;
-                
-            if(pisoDestino >= -2 && pisoDestino <= 11)
+            }
+            else if(pisoDestino >= -2 && pisoDestino <= 11)
+            {
             PisoActual = pisoDestino;
+            }
             return true;
         }
 
@@ -57,47 +61,56 @@ namespace Clase_4
             return personasBajadas;
         }
 
+        public int CantEsperada(int cantidad)
+        {
+            if(Capacidad > PersonasDentro)
+            {
+                cantidad += PersonasDentro;
+            }
+            return cantidad;
+        }
+
         public override string ToString()
         {
             return $"Piso: {PisoActual}, Personas: {PersonasDentro}";
         }
 
-        public enum Movimiento
-        {
-            Sub_II,
-            Sub_I,
-            Cero,
-            I,
-            II,
-            III,
-            IV,
-            V,
-            VI,
-            VII,
-            VIII,
-            IX,
-            X,
-            Error
-        }
-        public Movimiento movimiento()
-        {
-            return PisoActual switch
-            {
-                0 => Movimiento.Sub_II,
-                1 => Movimiento.Sub_I,
-                2 => Movimiento.Cero,
-                3 => Movimiento.I,
-                4 => Movimiento.II,
-                5 => Movimiento.III,
-                6 => Movimiento.IV,
-                7 => Movimiento.V,
-                8 => Movimiento.VI,
-                9 => Movimiento.VII,
-                10 => Movimiento.VIII,
-                11 => Movimiento.IX,
-                12 => Movimiento.X,
-                _ => Movimiento.Error
-            };
-        }
+        // public enum Movimiento
+        // {
+        //     Sub_II,
+        //     Sub_I,
+        //     Cero,
+        //     I,
+        //     II,
+        //     III,
+        //     IV,
+        //     V,
+        //     VI,
+        //     VII,
+        //     VIII,
+        //     IX,
+        //     X,
+        //     Error
+        // }
+        // public Movimiento movimiento()
+        // {
+        //     return PisoActual switch
+        //     {
+        //         0 => Movimiento.Sub_II,
+        //         1 => Movimiento.Sub_I,
+        //         2 => Movimiento.Cero,
+        //         3 => Movimiento.I,
+        //         4 => Movimiento.II,
+        //         5 => Movimiento.III,
+        //         6 => Movimiento.IV,
+        //         7 => Movimiento.V,
+        //         8 => Movimiento.VI,
+        //         9 => Movimiento.VII,
+        //         10 => Movimiento.VIII,
+        //         11 => Movimiento.IX,
+        //         12 => Movimiento.X,
+        //         _ => Movimiento.Error
+        //     };
+        // }
     }
 }
